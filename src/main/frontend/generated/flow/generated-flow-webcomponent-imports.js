67ae7be4c99226724653f46bf5b7be3336617c3a
darius1973 <darius.nica@gmail.com>
2024-09-29 17:23:26 +0200

    Build application for production

---
+++ src/main/frontend/generated/flow/generated-flow-webcomponent-imports.js
@@ -0,0 +1,25 @@
+import { injectGlobalWebcomponentCss } from 'Frontend/generated/jar-resources/theme-util.js';
+
+import '@vaadin/common-frontend/ConnectionIndicator.js';
+import '@vaadin/polymer-legacy-adapter/style-modules.js';
+import '@vaadin/vaadin-lumo-styles/sizing.js';
+import '@vaadin/vaadin-lumo-styles/spacing.js';
+import '@vaadin/vaadin-lumo-styles/style.js';
+import '@vaadin/vaadin-lumo-styles/vaadin-iconset.js';
+
+const loadOnDemand = (key) => {
+  const pending = [];
+  if (key === '485fcb5b172bcb7cb304c4db9d47b0fd45a0b4034075d29021049f46de190d3f') {
+    pending.push(import('./chunks/chunk-7529f626eb913774b3f6d7e23ab36b245b14eaeb71aa70634afb84a6e6016df2.js'));
+  }
+  return Promise.all(pending);
+}
+
+window.Vaadin = window.Vaadin || {};
+window.Vaadin.Flow = window.Vaadin.Flow || {};
+window.Vaadin.Flow.loadOnDemand = loadOnDemand;
+window.Vaadin.Flow.resetFocus = () => {
+ let ae=document.activeElement;
+ while(ae&&ae.shadowRoot) ae = ae.shadowRoot.activeElement;
+ return !ae || ae.blur() || ae.focus() || true;
+}
